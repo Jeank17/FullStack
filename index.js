67d86a20b38151b3f8f4cc80d154b153c99ed38b
Jean <jeanflog10@gmail.com>
2021-07-26 20:47:45 -0500

    Se agrega archivo html

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-//_____________________//
 console.log("Hola");
+
+let x = 2;
+console.log(typeof x);
+
+const y = 'hola';
+//y = 'hola' // no se puede reasignar si es const
+console.log(typeof y);
+
+if (-5){
+    console.log("entro al if");
+}else{
+    console.log("No entro")
+}
